39ffd4b13c6ebe44dcb3c7f6f486f26d4102ea1c
sergey.lukyanov <sergey.lukyanov@simbirsoft.com>
2019-07-14 19:42:11 +0400

    fixed errors in test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -5,8 +5,15 @@ from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    # link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
     link = 'http://selenium1py.pythonanywhere.com'
+    main_page = MainPage(browser, link)
+    main_page.open()
+    login_page = main_page.go_to_login_page()
+    login_page.should_be_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
     main_page = MainPage(browser, link)
     main_page.open()
     main_page.should_be_login_link()
