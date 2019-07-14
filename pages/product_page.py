6a5ead6b8c000054b56a40dfb455a7da9080442c
sergey.lukyanov <sergey.lukyanov@simbirsoft.com>
2019-07-14 21:28:19 +0400

    fixed product_page methods structure

--- pages/product_page.py
+++ pages/product_page.py
@@ -8,24 +8,22 @@ class ProductPage(BasePage):
         add_to_basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         add_to_basket_button.click()
 
-    def should_be_present_success_message(self):
+    def should_be_present_success_message(self, product_name=None):
         """Проверяет что сообщение об успешности добавления товара в корзину отображается
         """
         assert self.is_element_present(*ProductPageLocators.MESSAGE_SUCCSESS), 'Success alert is not presented'
-        product_name = self.get_product_name()
-        expected_text = '{} has been added to your basket.'.format(product_name)
+        if not product_name:
+            product_name = self.get_product_name()
         alert = self.browser.find_element(*ProductPageLocators.MESSAGE_SUCCSESS)
-        assert alert.text == expected_text, '{} != {}'.format(
-            alert.text, expected_text)
+        assert product_name in alert.text, f'Product name({product_name}) should be in alert.text({alert.text}) but not'
 
-    def should_be_present_cost_basket_message(self):
+    def should_be_present_cost_basket_message(self, product_price=None):
         assert self.is_element_present(
             *ProductPageLocators.MESSAGE_CHANGE_COST_BASKET), 'Success alert is not presented'
-        price = self.get_product_price()
-        expected_text = 'Your basket total is now {}'.format(price)
+        if not product_price:
+            product_price = self.get_product_price()
         alert = self.browser.find_element(*ProductPageLocators.MESSAGE_CHANGE_COST_BASKET)
-        assert alert.text == expected_text, '{} != {}'.format(
-            alert.text, expected_text)
+        assert product_price in alert.text, f'Price({product_price}) should be in alert.text({alert.text}) but not'
 
     def get_product_name(self) -> str:
         return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_LABEL).text
