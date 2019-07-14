4e372b00dd5acbf80ae6fade22f5ba7ec834dbb1
sergey.lukyanov <sergey.lukyanov@simbirsoft.com>
2019-07-14 14:57:19 +0400

    added should_be assert

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,13 +1,22 @@
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
 
 
 class BasePage:
 
-    def __init__(self, browser: RemoteWebDriver, url: str):
+    def __init__(self, browser: RemoteWebDriver, url: str, timeout: int = 10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         """Открывает страницу через url
         """
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
