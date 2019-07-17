3a87bb8089b5bf1467f7b13f4c45b7c833dac842
sergey.lukyanov <sergey.lukyanov@simbirsoft.com>
2019-07-18 03:23:26 +0400

    fixed transitions between pages

--- test_main_page.py
+++ test_main_page.py
@@ -14,7 +14,8 @@ class TestLoginFromMainPage(object):
         link = 'http://selenium1py.pythonanywhere.com'
         main_page = MainPage(browser, link)
         main_page.open()
-        login_page = LoginPage(browser, link + '/accounts/login/')
+        main_page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
         login_page.open()
         login_page.should_be_login_page()
 
@@ -30,7 +31,7 @@ def test_guest_cant_see_product_in_cart_opened_from_main_page(browser):
     main_page = MainPage(browser, link)
     main_page.open()
     main_page.go_to_cart_page()
-    cart_page = CartPage(browser, link)
+    cart_page = CartPage(browser, browser.current_url)
     cart_page.should_be_cart_empty()
     expected_text = 'Your basket is empty. Continue shopping'
     cart_page.should_be_present_message(expected_text)
--- test_product_page.py
+++ test_product_page.py
@@ -55,7 +55,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
-    login_page = LoginPage(browser, link)
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
 
@@ -65,7 +65,7 @@ def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_cart_page()
-    cart_page = CartPage(browser, link)
+    cart_page = CartPage(browser, browser.current_url)
     cart_page.should_be_cart_empty()
     expected_text = 'Your basket is empty. Continue shopping'
     cart_page.should_be_present_message(expected_text)
